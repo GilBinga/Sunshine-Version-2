86aa6c949f86d31c54acac3c8a91c0333b8531fb
Gil Binga <eng.binga@gmail.com>
2016-01-13 22:16:15 -0300

    Adiciona o conector HTTP em uma thread main
    Adiciona configuração de Proxy no Gradle

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.sunshine.app;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,6 +12,12 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -96,6 +103,68 @@ public class MainActivity extends ActionBarActivity {
 
             //Anexa o adpter ao ListView
             listView.setAdapter(mForecastAdapter);
+
+            //Implementação da comunicação HTTP
+            //Declaração de variáveis fora do try/catch para serem fechadas no bloco finally
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            //Vai conter a linha resposta JSON como uma String
+            String forecastJsonStr = null;
+
+            try {
+                //construi a URL para o OpenWheatherMap
+                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?id=3467747&mode=json&units=metric&cnt=7");
+
+                //Cria o request para o OpenWheaterMap e abre a conexão
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                //Cria e grava a InputStream em uma String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+
+                if (inputStream == null) {
+                    //Nada a fazer
+                    forecastJsonStr = null;
+                }
+
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+
+                while ((line = reader.readLine()) != null) {
+                    //Fazendo isso para ficar mais fácil o debugging
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    //Stream vazio
+                    forecastJsonStr = null;
+                }
+
+                forecastJsonStr = buffer.toString();
+
+            } catch (IOException e) {
+                Log.e("PlaceholderFragment", "Error", e);
+                //Se o código não conseguiu pegar o dado corretamente, garante que a variável
+                //forecastJsonStr fica em null
+                forecastJsonStr = null;
+
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e("PlaceholderFragment", "Error closing stream", e);
+                    }
+
+                }
+            }
 
 
 
